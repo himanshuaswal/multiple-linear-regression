03b68c40a170df803b6941beadc44de76d029a47
himanshu aswal <haswal403@gmail.com>
2018-11-10 17:32:56 +0530

    load the data from Boston house-prices dataset

--- multiple_regression_model.py
+++ multiple_regression_model.py
@@ -1,2 +1,6 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_boston
+# Load the data from the boston house-prices dataset
+boston_data = load_boston()
+x = boston_data['data']
+y = boston_data['target']
